9a650451b4e4b612d13812e82edfe3a00db813eb
Michael Rutto <michaelrutto9@gmail.com>
2026-01-08 08:25:42 +0300

    Implement currency conversion functionality

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,9 +1,12 @@
+/* eslint-disable react-hooks/set-state-in-effect */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { useEffect, useState } from 'react'
 import './App.css'
 import axios from 'axios';
 
 function App() {
+  const [usdValue, setUSDValue] = useState(0);
+  const [currency, setCurrency] = useState('GBP');
   const [conversions, setConversions] = useState([]);
 
   const getCurrencies = async () => {
@@ -13,7 +16,12 @@ function App() {
   }
 
   const convertCurrency = async () => {
+    const reposnse = await axios.post('http://localhost:8004/api/convert-currency', {
+      usd_value: usdValue,
+      currency
+    });
 
+    setConversions(reposnse.data.data);
   }
 
   useEffect(() => {
@@ -28,11 +36,18 @@ function App() {
         <div className=''>
           <div className='form-group'>
             <label>USD Currency</label>
-            <input type='number' />
+            <input
+              type='number'
+              value={usdValue}
+              onChange={(e) => setUSDValue(Number(e.target.value))}
+            />
           </div>
           <div className='form-group'>
             <label>Target Currency</label>
-            <select>
+            <select
+              value={currency}
+              onChange={(e) => setCurrency(e.target.value)}
+            >
               <option value={'KES'}>Kenya Shillings</option>
               <option value={'GBP'}>British Pound</option>
               <option value={'INR'}>Indian Rupee</option>
@@ -56,7 +71,7 @@ function App() {
                 <tr key={conversion.id}>
                   <th>USD {conversion.usd_value}</th>
                   <th>{conversion.target_currency} {conversion.target_value}</th>
-                  <th>{conversion.id}</th>
+                  <th>{conversion.created_at}</th>
                 </tr>
               ))}
             </tbody>
